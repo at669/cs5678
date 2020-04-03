0abc9e4069233e54bed7a7b093ea35af08375f62
Amanda Trang <at669@cornell.edu>
2020-04-03 18:44:41 -0400

    Knob control base scripts

--- Project/Assets/Scripts/ArduinoReader.cs
+++ Project/Assets/Scripts/ArduinoReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
@@ -27,45 +28,14 @@ public class ArduinoReader : MonoBehaviour
         stream.BaseStream.Flush();
     }
 
-    // public string ReadFromArduino (int timeout = 0) {
-    //     stream.ReadTimeout = timeout;        
-    //     try {
-    //         return stream.ReadLine();
-    //     }
-    //     catch (TimeoutException e) {
-    //         return null;
-    //     }
-    // }
-
-    public IEnumerator AsynchronousReadFromArduino(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity) {
-        DateTime initialTime = DateTime.Now;
-        DateTime nowTime;
-        TimeSpan diff = default(TimeSpan);
-
-        string dataString = null;
-
-        do {
-            try {
-                dataString = stream.ReadLine();
-            }
-            catch (TimeoutException) {
-                dataString = null;
-            }
-
-            if (dataString != null)
-            {
-                callback(dataString);
-                yield break; // Terminates the Coroutine
-            } else
-                yield return null; // Wait for next frame
-
-            nowTime = DateTime.Now;
-            diff = nowTime - initialTime;
-
-        } while (diff.Milliseconds < timeout);
-
-        if (fail != null)
-            fail();
-        yield return null;
+    public string ReadFromArduino (int timeout = 0) {
+        stream.ReadTimeout = timeout;        
+        try {
+            return stream.ReadLine();
+        }
+        catch (TimeoutException e) {
+            return null;
+        }
     }
+
 }
---
+++ Project/Assets/Scripts/KnobController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class KnobController : MonoBehaviour
+{
+    private List<GameObject> KnobList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KnobList = GameObject.FindGameObjectsWithTag("Knob").ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateKnobs(float[] val){
+        for (int i = 0; i < KnobList.Count; i++){
+            KnobList[i].transform.rotation = new Quaternion(KnobList[i].transform.rotation.x, KnobList[i].transform.rotation.y, val[i], 1);
+        }
+    }
+}
