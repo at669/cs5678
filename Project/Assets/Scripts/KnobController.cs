c8b6e6d297acccaee5131260634e662458b1bf3f
Amanda Trang <at669@cornell.edu>
2020-04-14 14:36:00 -0400

    Knobs working, glow

--- Project/Assets/Scripts/KnobController.cs
+++ Project/Assets/Scripts/KnobController.cs
@@ -5,23 +5,55 @@ using System.Linq;
 
 public class KnobController : MonoBehaviour
 {
+    private Transform EnvParent;
     private List<GameObject> KnobList = new List<GameObject>();
+    private List<Light> GlowList = new List<Light>();
+    private float[] tmpTurns = new float[]{0, 0, 0, 0};
 
     // Start is called before the first frame update
     void Start()
     {
-        KnobList = GameObject.FindGameObjectsWithTag("Knob").ToList();
+        EnvParent = GameObject.FindGameObjectWithTag("EnvParent").transform;
+        for (int i = 0; i < EnvParent.Find("StoveParent/Stove/Knobs").transform.childCount; i++){
+            KnobList.Add(EnvParent.Find("StoveParent/Stove/Knobs").transform.GetChild(i).gameObject);
+        }
+        for (int i = 0; i < EnvParent.Find("StoveParent/Stove/Glows").transform.childCount; i++){
+            GlowList.Add(EnvParent.Find("StoveParent/Stove/Glows").transform.GetChild(i).GetComponent<Light>());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A)){
+            tmpTurns[0] += 1;
+            tmpTurns = UpdateKnobs(tmpTurns);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            tmpTurns[1] += 1;
+            tmpTurns = UpdateKnobs(tmpTurns);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            tmpTurns[2] += 1;
+            tmpTurns = UpdateKnobs(tmpTurns);
+        }
+        if (Input.GetKey(KeyCode.F)){
+            tmpTurns[3] += 1;
+            tmpTurns = UpdateKnobs(tmpTurns);
+        }
+        for (int i = 0; i < tmpTurns.Length; i++){
+            Debug.Log(i + " " + tmpTurns[i] + " " + KnobList[i].transform.localEulerAngles.z);
+        }
     }
 
-    public void UpdateKnobs(float[] val){
+    public float[] UpdateKnobs(float[] val){
         for (int i = 0; i < KnobList.Count; i++){
-            KnobList[i].transform.rotation = new Quaternion(KnobList[i].transform.rotation.x, KnobList[i].transform.rotation.y, val[i], 1);
+            // KnobList[i].transform.Rotate(KnobList[i].transform.rotation.x, KnobList[i].transform.rotation.y, val[i])
+            KnobList[i].transform.Rotate(0, 0, val[i]);
+            // Debug.Log("val " + val[i] + " " + KnobList[i].transform.eulerAngles);
+            GlowList[i].intensity = KnobList[i].transform.localEulerAngles.z / 360 * 500;
+            val[i] = 0;
         }
+        return val;
     }
 }
